760525e1dc83eeab11b6e40e44aee0b3abc6a6f0
Ricardo Cacheira <ricardo.cacheira@hotmail.com>
2023-05-22 11:22:06 +0100

    TouchManager (States and current link)

--- Assets/Scritps/TouchManager.cs
+++ Assets/Scritps/TouchManager.cs
@@ -1,22 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
+
+public enum TouchState
+{
+    Idle,
+    Holding,
+    Released
+}
 
 public class TouchManager : MonoBehaviour
 {
     public LayerMask sphereLayer;
+    [SerializeField]
+    private TouchState state;
+    [SerializeField]
+    private List<Sphere> link;
     private Camera mainCamera;
+    private SColor current;
 
     private void Awake()
     {
         mainCamera = Camera.main;
+        link = new List<Sphere>();
+        state = TouchState.Idle;
+        current.id = -1;
     }
 
     void Update()
     {
         //Input.GetTouch
-        if (Input.GetMouseButton(0))
+        if (state != TouchState.Released && Input.GetMouseButton(0))
         {
+            if(state == TouchState.Idle)
+                state = TouchState.Holding;
+
             Vector3 pointerPosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
             RaycastHit hit;
             if (Physics.Raycast(pointerPosition, mainCamera.transform.TransformDirection(Vector3.forward), out hit, 15, sphereLayer))
@@ -25,15 +44,35 @@ public class TouchManager : MonoBehaviour
                 Sphere sphere = hit.collider.GetComponent<Sphere>();
                 if(sphere != null)
                 {
-                    Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
-                    Debug.Log("Did Hit");  
+                    // Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+                    // Debug.Log("Did Hit");
+
+                    if(link.Count > 0)
+                    {
+                        if((Vector3.Distance(link.Last().transform.position, sphere.transform.position) < 1.42f) && sphere.GetColor().id == current.id)
+                        {
+                            if(!link.Contains(sphere))
+                            {
+                                link.Add(sphere);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        current = sphere.GetColor();
+                        link.Add(sphere);
+                    }
                 }
             }
             else
             {
-                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
-                Debug.Log("Did not Hit");
+                // Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+                // Debug.Log("Did not Hit");
             }
+        }else if(state == TouchState.Holding)
+        {
+            state = TouchState.Released;
+            Debug.Log("score: " + link.Count);
         }
     }
 }
